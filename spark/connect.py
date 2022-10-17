36692a7867f7e99d55ef744029d62f9b6cd1c775
lucajanas <58073358+lucajanas@users.noreply.github.com>
2022-10-18 00:47:11 +0200

    Update connect.py

--- spark/connect.py
+++ spark/connect.py
@@ -1,23 +1,21 @@
+import warnings
+warnings.filterwarnings('ignore')
+
 import sys
 
 print(sys.executable)
+
+import os
+os.environ["PYSPARK_PYTHON"] = sys.executable
 
 import pyspark
 from pyspark.sql import SparkSession
 
 conf = pyspark.SparkConf().setMaster('spark://localhost:7077')
 spark = SparkSession \
-    .builder.config(conf=conf) \
+    .builder \
     .appName("Python") \
     .getOrCreate()
-
-# sc = pyspark.SparkContext(conf=conf)
-# rdd = spark.sparkContext.textFile("file:///tmp/ts_data_1.csv")
-# df = rdd.toDF()
-# from pyspark import SparkFiles
-
-# df = spark.sparkContext.textFile(SparkFiles.get('/tmp/ts_data_1.csv'))
-# df.show()
 
 print('Submitted application!')
 
@@ -26,17 +24,21 @@ import pandas as pd
 path = "C:\\Users\\Luca\\PycharmProjects\\distributed-systems\\load_simulation_data\\ts_data_1.csv"
 df_pd = pd.read_csv(path)
 
+print("Read csv via pandas")
+
 print(df_pd.head())
 
 sparkDF = spark.createDataFrame(df_pd)
+print("Converted pandas DataFrame to spark DataFrame")
 sparkDF.show()
 
-#df = spark.read.format("csv") \
-#    .option('header', True) \
-#    .option('multiLine', True) \
-#    .load(path)
-# .load(path)
+df = spark.read.format("csv") \
+    .option('header', True) \
+    .option('multiLine', True) \
+    .load(path)
 
-# df.show()
+print("Read csv directly via spark")
+
+df.show()
 
 print("Read data")
