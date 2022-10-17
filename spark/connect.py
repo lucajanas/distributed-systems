7d1c1282780b9b7266ed238427c3b3aa1131100f
lucajanas <l.janas@web.de>
2022-10-18 00:06:11 +0200

    small example

--- spark/connect.py
+++ spark/connect.py
@@ -1,6 +1,42 @@
-import pyspark
+import sys
 
-conf = pyspark.SparkConf().setAppName('MyApp').setMaster('spark://localhost:7077')
-sc = pyspark.SparkContext(conf=conf)
+print(sys.executable)
+
+import pyspark
+from pyspark.sql import SparkSession
+
+conf = pyspark.SparkConf().setMaster('spark://localhost:7077')
+spark = SparkSession \
+    .builder.config(conf=conf) \
+    .appName("Python") \
+    .getOrCreate()
+
+# sc = pyspark.SparkContext(conf=conf)
+# rdd = spark.sparkContext.textFile("file:///tmp/ts_data_1.csv")
+# df = rdd.toDF()
+# from pyspark import SparkFiles
+
+# df = spark.sparkContext.textFile(SparkFiles.get('/tmp/ts_data_1.csv'))
+# df.show()
 
 print('Submitted application!')
+
+import pandas as pd
+
+path = "C:\\Users\\Luca\\PycharmProjects\\distributed-systems\\load_simulation_data\\ts_data_1.csv"
+df_pd = pd.read_csv(path)
+
+print(df_pd.head())
+
+sparkDF = spark.createDataFrame(df_pd)
+sparkDF.show()
+
+df = spark.read.format("csv") \
+    .option('header', True) \
+    .option('multiLine', True) \
+    .load(path)
+# .load(path)
+
+# df.show()
+
+print("Read data")
